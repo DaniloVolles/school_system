f2182b70dccee872a8f1e2b07c6b9633d4cc6c06
Danilo Volles <danilo.volles@outlook.com>
2024-08-26 09:54:18 -0300

    feat: teacher controller implementation

---
+++ src/main/kotlin/com/danilovolles/schoolsystem/controller/TeacherController.kt
@@ -0,0 +1,40 @@
+package com.danilovolles.schoolsystem.controller
+
+import com.danilovolles.schoolsystem.dto.ApiResponseDTO
+import com.danilovolles.schoolsystem.dto.TeacherInputDTO
+import com.danilovolles.schoolsystem.dto.UserInputDTO
+import com.danilovolles.schoolsystem.service.TeacherService
+import jakarta.validation.Valid
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@RestController
+@RequestMapping("/teachers")
+class TeacherController {
+
+    @Autowired
+    final lateinit var teacherService: TeacherService
+
+    @PostMapping("/new")
+    fun createTeacher(@Valid @RequestBody teacherData: TeacherInputDTO) = teacherService.createTeacher(teacherData)
+
+    @GetMapping("/get/all")
+    fun getAllTeachers() = teacherService.getAllTeacher()
+
+    @GetMapping("/get/{teacherId}")
+    fun getTeacherById(@Valid @PathVariable teacherId: UUID) = teacherService.getTeacherById(teacherId)
+
+    @GetMapping("/get/subj/{subjectName}")
+    fun getTeacherBySubject(@PathVariable subjectName: String) = teacherService.getTeacherBySubject(subjectName)
+
+    @PutMapping("/update/{teacherId}")
+    fun updateTeacherById(
+        @Valid @PathVariable teacherId: UUID,
+        @RequestBody teacherData: TeacherInputDTO
+    ) = teacherService.updateTeacher(teacherId, teacherData)
+
+    @PutMapping("/inactive/{teacherId}")
+    fun inactiveTeacherById(@Valid @PathVariable teacherId: UUID) = teacherService.inactiveTeacher(teacherId)
+}
