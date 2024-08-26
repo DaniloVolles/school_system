ed5dda52e9f0ba9cc82ed7950ada6abf7b9d32e9
Danilo Volles <danilo.volles@outlook.com>
2024-08-26 09:36:14 -0300

    feat: teacher class

---
+++ src/main/kotlin/com/danilovolles/schoolsystem/entity/Teacher.kt
@@ -0,0 +1,18 @@
+package com.danilovolles.schoolsystem.entity
+
+import jakarta.persistence.*
+import java.util.UUID
+
+@Entity
+@Table(name = "tb_teachers")
+data class Teacher(
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    val id: UUID,
+
+    var name: String,
+    var email: String,
+    var password: String,
+    var subject: String,
+    var active: Boolean
+)
