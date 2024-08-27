6c851d86f6c582d111da0bec32e267afd0e74340
Danilo Volles <danilo.volles@outlook.com>
2024-08-27 09:03:56 -0300

    feat: student class and repository

---
+++ src/main/kotlin/com/danilovolles/schoolsystem/entity/Student.kt
@@ -0,0 +1,18 @@
+package com.danilovolles.schoolsystem.entity
+
+import jakarta.persistence.*
+import java.util.UUID
+
+@Entity
+@Table(name = "tb_student")
+data class Student(
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    val id: UUID,
+
+    var name: String,
+    var email: String,
+    var password: String,
+    var active: Boolean,
+    var schoolClass: String
+)
---
+++ src/main/kotlin/com/danilovolles/schoolsystem/repository/StudentRepository.kt
@@ -0,0 +1,9 @@
+package com.danilovolles.schoolsystem.repository
+
+import com.danilovolles.schoolsystem.entity.Student
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface StudentRepository : JpaRepository<Student, UUID> {
+    fun findStudentBySchoolClass(schoolClass: String): Student?
+}
