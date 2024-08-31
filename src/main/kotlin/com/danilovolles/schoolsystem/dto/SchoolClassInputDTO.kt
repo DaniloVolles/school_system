f924d223afbe090d76f8043fd44144113961fd90
Danilo Volles <danilo.volles@outlook.com>
2024-08-31 18:40:37 -0300

    feat: validators in DTO

--- src/main/kotlin/com/danilovolles/schoolsystem/dto/SchoolClassInputDTO.kt
+++ src/main/kotlin/com/danilovolles/schoolsystem/dto/SchoolClassInputDTO.kt
@@ -1,10 +1,18 @@
 package com.danilovolles.schoolsystem.dto
 
 import com.danilovolles.schoolsystem.entity.Teacher
+import jakarta.validation.constraints.NotBlank
 
 data class SchoolClassInputDTO(
+
+    @NotBlank(message = "Name must be provided")
     var name: String,
+
+    @NotBlank(message = "Description must be provided")
     var description: String,
+
+    @NotBlank(message = "Subject must be provided")
     var subject: String,
+
     var teacher: Teacher?
 )
